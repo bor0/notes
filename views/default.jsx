825d03ecf05183b00d3010c27421f18d5f04f0b7
Boro Sitnikovski <buritomath@gmail.com>
2017-03-12 22:30:30 +0100

    Move the default view to be a function instead of a class.

--- views/default.jsx
+++ views/default.jsx
@@ -1,25 +1,22 @@
 import React from 'react';
 
-const defaultview = React.createClass( {
+const defaultView = () => {
+	return (
+		<html>
+			<head>
+				<meta charSet="utf-8"></meta>
+				<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1"></meta>
+				<title>Notes</title>
+				<link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css" rel="stylesheet"></link>
+				<link href='https://fonts.googleapis.com/css?family=Varela+Round' rel='stylesheet' type='text/css'></link>
+				<link href='css/style.css' rel='stylesheet' type='text/css'></link>
+			</head>
+			<body>
+				<div id="root"></div>
+				<script src="js/bundle.js"></script>
+			</body>
+		</html>
+	);
+};
 
-	render: function() {
-		return (
-			<html>
-				<head>
-					<meta charSet="utf-8"></meta>
-					<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1"></meta>
-					<title>Notes</title>
-					<link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css" rel="stylesheet"></link>
-					<link href='https://fonts.googleapis.com/css?family=Varela+Round' rel='stylesheet' type='text/css'></link>
-					<link href='css/style.css' rel='stylesheet' type='text/css'></link>
-				</head>
-				<body>
-					<div id="root"></div>
-					<script src="js/bundle.js"></script>
-				</body>
-			</html>
-		);
-	}
-} );
-
-module.exports = defaultview;
+export default defaultView;
